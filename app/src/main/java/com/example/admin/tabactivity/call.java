8e1e31eb0e6b8bbc6b7222940a4b40566d68b750
vineetha19 <42667307+vineetha19@users.noreply.github.com>
2018-10-08 04:33:13 -0700

    tabs

---
+++ app/src/main/java/com/example/admin/tabactivity/call.java
@@ -0,0 +1,28 @@
+package com.example.admin.tabactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class call extends Fragment {
+
+    public call() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_call, container, false);
+    }
+
+}
